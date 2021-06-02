6a6e18d63233c07473229b27a2f44418230ba819
Haven <haventh@163.com>
2021-06-02 20:16:29 +0800

    init

---
+++ src/main/java/com/sc_gatewaydemo/ScGatewayDemoApplication.java
@@ -0,0 +1,13 @@
+// package com.sc_gatewaydemo;
+//
+// import org.springframework.boot.SpringApplication;
+// import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+// @SpringBootApplication
+// public class ScGatewayDemoApplication {
+//
+//     public static void main(String[] args) {
+//         SpringApplication.run(ScGatewayDemoApplication.class, args);
+//     }
+//
+// }
---
+++ src/test/java/com/sc_gatewaydemo/ScGatewayDemoApplicationTests.java
@@ -0,0 +1,13 @@
+// package com.sc_gatewaydemo;
+//
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
+//
+// @SpringBootTest
+// class ScGatewayDemoApplicationTests {
+//
+//     @Test
+//     void contextLoads() {
+//     }
+//
+// }
